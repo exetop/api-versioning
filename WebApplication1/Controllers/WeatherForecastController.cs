5f8b57e85120514a80a648406ecae2c0c95620ea
Alexander Maksimov <exetop@gmail.com>
2021-07-06 08:57:45 +0300

    Add project files.

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    public class TestClass
+    {
+        public int Number { get; set; }
+    }
+
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("[controller]")]
+    public class WeatherForecastController : Controller
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [ApiVersion("1.1")]
+        public IEnumerable<TestClass[]> Get2()
+        {
+            yield return new TestClass[]
+            {
+                new TestClass { Number=1},
+                new TestClass { Number=2}
+            };
+        }
+    }
+
+    [ApiController]
+    [ApiVersion("2.0")]
+    [Route("[controller]")]
+    public class NumsController : Controller
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public NumsController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<long[]> Get()
+        {
+            yield return new long[] { 1,2,3};
+        }
+    }
+}
---
+++ WebApplication1/ServicesExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace WebApplication1
+{
+    public static class ServicesExtensions
+    {
+        public static void AddApiVersioning(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.ApiVersionReader = new QueryStringApiVersionReader("version");
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+                options.UseApiBehavior = true; // It means include only api controller not mvc controller.
+                //options.Conventions.Controller<AppController>().HasApiVersion(options.DefaultApiVersion);
+                //options.Conventions.Controller<UserController>().HasApiVersion(options.DefaultApiVersion);
+                options.ApiVersionSelector = new CurrentImplementationApiVersionSelector(options);
+            });
+
+            services.AddVersionedApiExplorer(); // It will be used to explorer api versioning and add custom text box in swagger to take version number.
+        }
+
+        public static void AddSwaggerGenerationUI(this IServiceCollection services)
+        {
+            var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+            
+            services.AddSwaggerGen(options =>
+            {
+                options.OrderActionsBy(orderBy => orderBy.HttpMethod);
+                //options.UseReferencedDefinitionsForEnums();
+                foreach (var item in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerDoc(item.GroupName, new OpenApiInfo
+                    {
+                        Title = "Version-" + item.GroupName,
+                        Version = item.ApiVersion.MajorVersion.ToString() + "." + item.ApiVersion.MinorVersion
+                    });
+                }
+            });
+        }
+
+        public static void UseSwaggerGenerationUI(this IApplicationBuilder applicationBuilder)
+        {
+            var provider = applicationBuilder.ApplicationServices.GetRequiredService<IApiVersionDescriptionProvider>();
+
+            applicationBuilder.UseSwagger();
+            applicationBuilder.UseSwaggerUI(c =>
+            {
+                c.DocumentTitle = "Api Help";
+                foreach (var item in provider.ApiVersionDescriptions)
+                {
+                    c.SwaggerEndpoint($"/swagger/{item.GroupName}/swagger.json", item.GroupName);
+                }
+            });
+        }
+    }
+}
